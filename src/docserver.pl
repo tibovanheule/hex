afbe871dcc8287d1beeb7c0bad6bc8d6dd47dc14
root <root@tibovanheule.space>
2020-04-30 16:39:21 +0200

    doc server script

--- src/docserver.pl
+++
@@ -1,3 +0,0 @@
-:- doc_server(4000).
-:- use_module(library(pldoc/doc_library)).
-:- doc_load_library.
---
+++ src/docserver.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env swipl
+
+:- doc_server(3021,[allow(localhost),edit(false)]).
+:- use_module(library(pldoc/doc_library)).
+:- doc_load_library.
