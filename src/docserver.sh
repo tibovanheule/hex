08942bdcaf50cc5f97bfbe73b825825741119f7e
root <root@tibovanheule.space>
2020-05-04 12:17:29 +0200

    Doc server werkend

--- src/docserver.sh
+++ src/docserver.sh
@@ -4,8 +4,6 @@
 Deze module start een documentatie server op, deze documentatie wordt opgebouwd aan de hand van structured comments..
 @author Tibo Vanheule
 */
-:- use_module(library(pldoc/doc_library)).
-
-:- doc_server(3021,[allow(localhost),edit(false)]).
-:- consult('server.sh'), consult('main.pl').
-:- doc_load_library.
+:- doc_server(3021,[edit(false)]).
+:- consult('main.pl').
+:- thread_get_message(Message).
